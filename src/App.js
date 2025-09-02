0e67c2d2c1d785d3d3fe96a668355e10c65ed119
NazarBunko <bunkonazar@gmail.com>
2025-09-02 15:16:10 +0300

    fix app js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./layout/header&footer/Header";
 import Footer from "./layout/header&footer/Footer";
 import Main from "./layout/Main";
@@ -11,12 +11,12 @@ function App() {
     <Router>
       <Header /> {/* хідер завжди зверху */}
       
-      <main style={{ minHeight: "calc(100vh - 128px)" }}> 
+      <main style={{ minHeight: "calc(100vh - 128px)" }}>
         {/* 128px = висота хідера + футера (підкоригуй) */}
         <Routes>
-          <Route path="/house24/" element={<Main />} />
-          <Route path="/house24/daily" element={<DailyPage />} />
-          <Route path="/house24/monthly" element={<MonthlyPage />} />
+          <Route path="/" element={<Main />} />
+          <Route path="/daily" element={<DailyPage />} />
+          <Route path="/monthly" element={<MonthlyPage />} />
         </Routes>
       </main>
 
